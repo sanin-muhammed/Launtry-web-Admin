46bd3f4e9ee9a5dc0856985b59adb905f074b274
Sanin Muhammed <saninmuhammedpt@gmail.com>
2024-06-05 22:09:23 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -1,27 +1,20 @@
-const express = require('express');
-const colors = require('colors');
-const app = express()
-const cors = require('cors');
-const morgan = require('morgan');
-const connectDB = require('./config/database');
-const router = require('./routes/authRoutes');
-const routes = require('./routes/routes');
+const express = require("express");
+const colors = require("colors");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+const connectDB = require("./config/database");
+const router = require("./routes/authRoutes");
+const routes = require("./routes/routes");
 const PORT = 2002;
-
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(cors())
-app.use(morgan('dev'));
-connectDB()
-
-
+app.use(cors());
+app.use(morgan("dev"));
+connectDB();
 
 app.use("/api", router);
 app.use("/api", routes);
 
-
-
-
-
-app.listen(PORT,()=> console.log("server running on port".bold,PORT));
+app.listen(PORT, () => console.log("server running on port".bold, PORT));
